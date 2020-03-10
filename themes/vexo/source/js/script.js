b534257b4852681d86fbf416d371e86e70664df6
Fancy <peifancy@gmail.com>
2020-03-11 01:52:33 +0800

    + 20.03.11.md

--- themes/vexo/source/js/script.js
+++ themes/vexo/source/js/script.js
@@ -11,7 +11,7 @@
   $(document).ready(function () {
     NProgress.start()
     $('#nprogress .bar').css({
-      'background': '#42b983'
+      'background': '#4caae8'
     })
     $('#nprogress .spinner').hide()
 
